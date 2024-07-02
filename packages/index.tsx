29d60e850e8a5e265ddd27f6a3b40bb8ab2209d6
fireant <societyhater2@gmail.com>
2024-07-02 17:41:32 +0545

    fix(): fixed pointer events in full variant

--- packages/index.tsx
+++ packages/index.tsx
@@ -13,6 +13,7 @@ const DocPreviewer = ({
 		if (show && variant === 'full') {
 			const ele = document.createElement('div');
 			ele.id = "react-doc-previwer-121";
+			ele.setAttribute('style', `'position:relative,overflow:hidden,pointer-events:auto,width:' ${rest.variant === "inherit" ? 'inherit' : rest.variant === "full" ? '100%' : rest.variant[0]}',height: '${rest.variant === "inherit" ? 'inherit' : rest.variant === "full" ? '100%' : rest.variant[1]}`)
 
 			const root = createRoot(ele);
 			close = () => { root.unmount(); ele.remove(); onClose() };
