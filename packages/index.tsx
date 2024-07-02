8ab15f1f0c0ee89d1767db3795981ffb6ae8c7b8
fireant <societyhater2@gmail.com>
2024-07-02 16:47:45 +0545

    feat(): added feature for render outsidethe react dom

--- packages/index.tsx
+++ packages/index.tsx
@@ -1,16 +1,42 @@
+import { useEffect } from 'react';
+import { createRoot } from 'react-dom/client'
 import { ImageCanvasProps, ImageCanvas } from './image';
 import { PDFCanvas } from './pdf';
 const DocPreviewer = ({
 	type,
 	...rest
 }: ImageCanvasProps & { type: "image" | "pdf" }) => {
-	return (
-		<div style={{
-			position: "relative",
-			overflow: "hidden",
-			width: rest.variant === "inherit" ? 'inherit' : rest.variant === "full" ? '100%' : rest.variant[0],
-			height: rest.variant === "inherit" ? 'inherit' : rest.variant === "full" ? '100%' : rest.variant[1],
-		}} >
+	const { show, variant, onClose } = rest;
+	let close = onClose;
+
+	useEffect(() => {
+		if (show && variant === 'full') {
+			const ele = document.createElement('div');
+			ele.id = "react-doc-previwer-121";
+			ele.setAttribute('style', 'position:fixed')
+
+			const root = createRoot(ele);
+			close = () => { root.unmount(); onClose() };
+			root.render(
+				type === "image" ?
+					<ImageCanvas {...rest} onClose={close} /> :
+					<PDFCanvas {...rest} onClose={close} />
+			);
+			document.body.appendChild(ele);
+			console.log("here");
+
+		}
+	}, [show])
+
+	variant !== 'full' &&
+		<div
+			id="react-doc-previewer-121"
+			style={{
+				position: "relative",
+				overflow: "hidden",
+				width: rest.variant === "inherit" ? 'inherit' : rest.variant === "full" ? '100%' : rest.variant[0],
+				height: rest.variant === "inherit" ? 'inherit' : rest.variant === "full" ? '100%' : rest.variant[1],
+			}} >
 			{
 				type === "image" ?
 					<ImageCanvas {...rest} /> :
@@ -18,7 +44,6 @@ const DocPreviewer = ({
 			}
 
 		</div>
-	)
 }
 
 export default DocPreviewer;
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     tsConfigPaths(),
     dts({
-      include: ['packages/'],
+      include: ['packages/index.tsx'],
     }),
   ],
   build: {
